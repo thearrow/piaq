0c69aa9b4468624ee4ff43f6615b1205ff37f24f
Jake Klingler <thearrow3456@gmail.com>
2019-07-01 17:36:40 -0400

    compensate ambient temp based on cpu temp

--- bme680_example.py
+++ bme680_example.py
@@ -3,37 +3,47 @@
 # https://github.com/pimoroni/bme680-python
 
 import bme680
-
-print(
-    """temperature-pressure-humidity.py - Displays temperature, pressure, and humidity.
-If you don't need gas readings, then you can read temperature,
-pressure and humidity quickly.
-Press Ctrl+C to exit
-"""
-)
+from time import sleep
+from subprocess import PIPE, Popen
 
 try:
     sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
 except IOError:
     sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
 
-# These oversampling settings can be tweaked to
-# change the balance between accuracy and noise in
-# the data.
-
-sensor.set_humidity_oversample(bme680.OS_2X)
-sensor.set_pressure_oversample(bme680.OS_4X)
+sensor.set_humidity_oversample(bme680.OS_8X)
+sensor.set_pressure_oversample(bme680.OS_8X)
 sensor.set_temperature_oversample(bme680.OS_8X)
 sensor.set_filter(bme680.FILTER_SIZE_3)
 
-print("Polling:")
+
+def get_cpu_temperature():
+    process = Popen(["vcgencmd", "measure_temp"], stdout=PIPE, text=True)
+    output, _error = process.communicate()
+    return float(output[output.index("=") + 1 : output.rindex("'")])
+
+
+factor = 10.0  # Smaller numbers adjust temp down, vice versa
+smooth_size = 10  # Dampens jitter due to rapid CPU temp changes
+cpu_temps = []
+
 try:
     while True:
         if sensor.get_sensor_data():
-            output = "{0:.2f} C,{1:.2f} hPa,{2:.3f} %RH".format(
-                sensor.data.temperature, sensor.data.pressure, sensor.data.humidity
+            cpu_temp = get_cpu_temperature()
+            cpu_temps.append(cpu_temp)
+            if len(cpu_temps) > smooth_size:
+                cpu_temps = cpu_temps[1:]
+
+            smoothed_cpu_temp = sum(cpu_temps) / float(len(cpu_temps))
+            raw_temp = sensor.data.temperature
+            comp_temp = raw_temp - ((smoothed_cpu_temp - raw_temp) / factor)
+
+            output = "{0:.2f}C, {1:.3f}%RH, {2:.2f}hPa".format(
+                comp_temp, sensor.data.humidity, sensor.data.pressure
             )
             print(output)
+        sleep(1)
 
 except KeyboardInterrupt:
     pass
